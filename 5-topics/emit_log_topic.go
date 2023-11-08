f44e3429e51ca4f204ed0d2a3bf9287b3bdbc5f9
ortizdavid <ortizaad1994@gmail.com>
2023-11-08 14:53:01 +0100

    Topics

--- 5-topics/emit_log_topic.go
+++ 5-topics/emit_log_topic.go
@@ -10,39 +10,40 @@ import (
 	amqp "github.com/rabbitmq/amqp091-go"
 )
 
-func failOnError(err error, msg string) {
+
+func failOnError(err error, msg string)  {
 	if err != nil {
-		log.Panicf("%s: %s", msg, err)
+		log.Fatalf("%s:%s", msg, err)
 	}
 }
 
 func main() {
 	
 	conn, err := amqp.Dial("amqp://guest:guest@localhost:5672/")
-	failOnError(err, "Failed to connect")
+	failOnError(err, "Failed to connect to RabbitMQ")
 	defer conn.Close()
 
 	ch, err := conn.Channel()
-	failOnError(err, "Failed to open a channel")
+	failOnError(err, "Failed to open channel")
 	defer ch.Close()
 
 	err = ch.ExchangeDeclare(
-		"logs_direct",
-		"direct",
+		"logs_topic",
+		"topic",
 		true,
 		false,
 		false,
 		false,
 		nil,
 	)
-	failOnError(err, "Failed to declare an exchange")
+	failOnError(err, "Filed to declare an exchange")
 
 	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 	defer cancel()
 
 	body := bodyFrom(os.Args)
 	err = ch.PublishWithContext(ctx,
-		"logs_direct",
+		"logs_topic",
 		severityFrom(os.Args),
 		false,
 		false,
@@ -50,7 +51,7 @@ func main() {
 			ContentType: "text/plain",
 			Body: []byte(body),
 		})
-	failOnError(err, "Failed to publish a message")
+	failOnError(err, "Fail to publish a message")
 
 	log.Printf(" [x] Sent %s", body)
 }
@@ -60,15 +61,15 @@ func bodyFrom(args []string) string {
 	if (len(args) < 3) || os.Args[2] == "" {
 		s = "hello"
 	} else {
-		s = strings.Join(args[2:], " ")
+		s = strings.Join(os.Args[2:], " ")
 	}
 	return s
 }
 
 func severityFrom(args []string) string {
 	var s string
-	if (len(args) <2) || os.Args[1] == "" {
-		s = "info"
+	if (len(args) < 2) || os.Args[1] == "" {
+		s = "anonymous.info"
 	} else {
 		s = os.Args[1]
 	}
